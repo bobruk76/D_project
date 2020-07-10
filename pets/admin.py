608756ddcb6cf5b03643a5ba19db7801b49c9a91
bobruk76 <vladimir.m.polyakov@gmail.com>
2020-07-10 10:17:04 +0300

    add admin

--- pets/admin.py
+++ pets/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import (Book, Author, Reader, BookReading, Publisher, BookPublishing)
 
-# Register your models here.
+# [admin.site.register(item) for item in (Book, Author, Reader, BookReading)]
+
+@admin.register(Aninal)
+class AuthorAdmin(admin.ModelAdmin):
+    pass
