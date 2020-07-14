dfb032f9a6f889f3a3be176d3a44c85703e4e40d
bobruk76 <vladimir.m.polyakov@gmail.com>
2020-07-14 16:53:13 +0300

    change fixtures to json

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
 release: python manage.py migrate
-release: python manage.py loaddata fixtures.xml
+release: python manage.py loaddata fixtures.json
 web: gunicorn petshome.wsgi
--- petshome/settings.py
+++ petshome/settings.py
@@ -77,19 +77,19 @@ WSGI_APPLICATION = 'petshome.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'petshome',
-#         'USER': 'petshomeuser',
-#         'PASSWORD': 'petshomeuser',
-#         'HOST': '192.168.168.242',
-#         'PORT': '5432',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'petshome',
+        'USER': 'petshomeuser',
+        'PASSWORD': 'petshomeuser',
+        'HOST': '192.168.168.242',
+        'PORT': '5432',
+    }
+}
 # Activate Django-Heroku.
-django_heroku.settings(locals())
-DATABASES = {'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))}
+# django_heroku.settings(locals())
+# DATABASES = {'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
