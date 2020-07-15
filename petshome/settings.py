d90bfcdd35b798736c58f43575a97ad1af237694
bobbruk76 <vladimir.m.polyakov@gmail.com>
2020-07-15 19:48:16 +0300

    ++

--- petshome/settings.py
+++ petshome/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'petshome.wsgi.application'
 
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'ENGINE': 'django.db.backends.postgresql',
 #         'NAME': 'petshome',
 #         'USER': 'petshomeuser',
 #         'PASSWORD': 'petshomeuser',
