71ec0b0a2ea681c58b3daf75d7128ec4a269d0cd
bobruk76 <vladimir.m.polyakov@gmail.com>
2020-07-15 13:46:00 +0300

    add mentors settings

--- petshome/settings.py
+++ petshome/settings.py
@@ -79,16 +79,16 @@ WSGI_APPLICATION = 'petshome.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'petshome',
-        'USER': 'petshomeuser',
-        'PASSWORD': 'petshomeuser',
-        'HOST': '192.168.168.242',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'petshome',
+#         'USER': 'petshomeuser',
+#         'PASSWORD': 'petshomeuser',
+#         'HOST': '192.168.168.242',
+#         'PORT': '5432',
+#     }
+# }
 
 # DATABASES = {
 #     'default': {
